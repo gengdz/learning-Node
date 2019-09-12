74c427494ff30605bf3daf5e38fabd457c922347
gengdz <1583751445@qq.com>
2019-09-13 00:24:10 +0800

    node处理静态资源

--- src/staticResource/app.js
+++ src/staticResource/app.js
@@ -6,19 +6,10 @@ const url = require('url')
 
 const comments = [
   {
-    name: 'gdz',
+    name: '耿德洲',
     message: '耿德洲发表了帖子',
     dateTime: '2019/9/12'
-  }, {
-    name: '张三',
-    message: '张三发表了帖子',
-    dateTime: '2019/9/12'
-  }, {
-    name: '李四',
-    message: '李四发表了帖子',
-    dateTime: '2019/9/12'
   },
-
 ]
 
 http.createServer((req, res) => {
@@ -43,25 +34,19 @@ http.createServer((req, res) => {
       if (err) return res.end('notFoundPage')
       res.end(data)
     })
-
   } else if (pathname === '/pinglun') {
-
     const comment = pathObj.query
     comment.dateTime = new Date()
-    comments.push(comment)
+    comments.unshift(comment)
 
     /**
      * 如何通过服务器让客户端重定向？
-     * 
+     * 状态码设置为302，临时重定向 res.statusCode = 302
+     * 在相应头中通过Location告诉客户端往哪里重定向 res.setHeader('Location', '/')
      */
-
-    fs.readFile('./views/index.html', (err, data) => {
-      if (err) return res.end('notFoundPage')
-      const htmlContent = template.render(data.toString, { comments })
-      res.end(htmlContent)
-    })
-
-
+    res.statusCode = 302
+    res.setHeader('Location', '/')
+    res.end()
   } else {
     fs.readFile('./views/404.html', (err, data) => {
       return res.end(data)
